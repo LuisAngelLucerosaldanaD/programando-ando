bb24f20ada41446a5c607e230820dbd387389eec
luis <luis.com>
2019-10-18 13:39:11 -0500

    login progrmando ando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  transition(){
+    const btnSubmit=document.getElementById('btnSubmit'),
+    left=document.getElementById('left');
+    btnSubmit.addEventListener('click',(e)=>{
+      e.preventDefault();
+      left.classList.add('activo');
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="left" id="left">
+          <div className="container-saludo">
+            <span>Programando Ando</span>
+          </div>
+          <div className="container-block">
+            <h1>Welcome Back</h1>
+            <p>To keep conected with us please login with your personal info.</p>
+            <button type="button" className="btnSubmit" name="btnSubmit" id="btnSubmit" onClick={this.transition}>SIGN IN</button>
+          </div>
+        </div>
+        <div className="rigth">
+          <div className="container-title">
+              <h1>Create Acount</h1>
+          </div>
+          <div className="enlases-nav">
+              <a href="facebook.com">f</a>
+              <a href="google.com">G+</a>
+              <a href="twiter.com">ln</a>
+          </div>
+          <div className="container-form">
+            <h4>of use your email for registration</h4>
+            <form className="form">
+              <input className="cajas" placeholder="Name"></input><br/>
+              <input className="cajas" placeholder="Email"></input><br/>
+              <input className="cajas" placeholder="Password"></input><br/>
+              <button id="btnSignUp" className="btnSignUp">SIGN UP</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
